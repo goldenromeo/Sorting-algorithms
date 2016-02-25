d95332413d53b7803652a70c0f5678e9bd404d3d
Rommel Trejo <rommeltrejo@gmail.com>
2016-02-24 22:09:44 -0500

    Create sort.java
    added insertion sort

---
+++ sort.java
@@ -0,0 +1,59 @@
+/**
+ * Created by rommeltrejo on 2/24/16.
+ */
+public class sort  {
+
+
+
+public static Comparable[] insertion (Comparable[] array){
+
+//initial loop on insertion sort  array= 0_1_2_3_4_5_6... i is place on array; array  starts at 0
+
+    //if array is empty then length = o,so loop is never executed
+
+    for (int i = 1; i < array.length;i++ ){
+
+        /**
+         * second loop; k is array index like above and > i
+         * k goes down
+         * this part of the method starts at i and checks that everything below(inclusive)
+         * it is in decresing order
+         *
+         * e.g.  2,3,1
+         * pass i=1: 2<3 : do nothing
+         * pass i=2: 3 !<1
+         *  k=2:  swap(3,1), 2,1,3,k--
+         *  k=1:  2 !< 1; swap(2,1), 1,2,3
+         *  (k !>= 1) exit loop
+         */
+
+        for ( int k = i; k>=1 && array[k].compareTo(array[k-1]) <0 ;k-- )
+        {
+            Comparable temp = array[k];
+            array[k]=array[k-1];
+            array[k-1]=temp;
+            temp=null;
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+    return array;
+
+}
+
+
+
+
+
+}
